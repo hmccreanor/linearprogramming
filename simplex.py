d77b8f44b185248aa9a5a97d8797657bd0dfb01a
McCreanor (P-L6) Hamish <15McCreanorH@harrowschool.org.uk>
2019-04-18 13:32:26 +0100

    Parser reworked + optimal variable extraction implemented

--- simplex.py
+++ simplex.py
@@ -1,6 +1,6 @@
 from math import inf
 import numpy as np
-from lExpParser import parse
+from lExpParser2 import Tableu
 import sys
 
 inFile = sys.argv[1]
@@ -8,13 +8,17 @@ inFile = sys.argv[1]
 # Supress divide by 0 warnings
 np.seterr(divide="ignore")
 
-tableu, slack_start_index = parse(inFile)
+t = Tableu(inFile)
+tableu = t.tableu
+slack_start_index = t.slack_start_index
+variables = t.variables
 
 constraint_index = tableu.shape[1] - 1
 bottom_index = tableu.shape[0] - 1
 
 variable_values = tableu[bottom_index][:slack_start_index]
 
+# Running the simplex algorithm
 while np.any(variable_values < 0): # Some negative values in left part of tableu
     pivot_column = np.argmin(variable_values)
 
@@ -33,6 +37,26 @@ while np.any(variable_values < 0): # Some negative values in left part of tableu
         if i != pivot_row and tableu[i][pivot_column] != 0:
             tableu[i] = ((-1 * tableu[i][pivot_column]) * tableu[pivot_row]) + tableu[i]
 
+print("Final tableu:")
 print(tableu)
+print(" ")
 
+# Extracting the values of the variable coefficients
+for i in range(slack_start_index):
+    column = tableu[:,i]
+    ones_count = 0
+    one_index = 0
+    exclusively_ones_and_zeros = True
+    for j in range(len(column)):
+        if column[j] == 1:
+            ones_count += 1
+            one_index = j
+        elif column[j] != 0:
+            exclusively_ones_and_zeros = False
+            break
+
+    if ones_count == 1 and exclusively_ones_and_zeros:
+        print(variables[i], ":", tableu[one_index][tableu.shape[1] - 1])
+    else:
+        print(variables[i], ": 0")
 
